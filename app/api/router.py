a6eeb48740b8c68f8a9dcc993b932553b39355c4
Samrawit-alemu <samrawittalemuu@gmail.com>
2026-02-07 09:42:11 -0800

    Add GET endpoints

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from typing import List
+from beanie import PydanticObjectId # To validate MongoDB IDs
 
 from app.models.job_model import Job
 from app.schemas.job_schema import JobCreate, JobResponse
@@ -20,3 +21,25 @@ async def create_job(job_input: JobCreate):
 
     # 3. Return the created job (FastAPI converts it to JobResponse automatically)
     return new_job
+
+@router.get("/jobs", response_model=List[JobResponse])
+async def get_all_jobs():
+    # 1. Query the database
+    # Job.find_all() prepares the query
+    # .to_list() executes it asynchronously
+    jobs = await Job.find_all().to_list()
+    return jobs
+
+@router.get("/jobs/{job_id}", response_model=JobResponse)
+async def get_job_by_id(job_id: PydanticObjectId): # in the parameter chck the id validity
+    # 1. search for the job
+    job = await Job.get(job_id)
+
+    # 2. Check if it exists
+    if not job:
+        # If None, return a 404 Error
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Job not found"
+        )
+    return job
